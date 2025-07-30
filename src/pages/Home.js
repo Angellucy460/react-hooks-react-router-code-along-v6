62c14002f517304b4ce9cb3799a7177138be4c20
Angellucy460 <angelliona38@gmail.com>
2025-07-30 20:02:01 +0300

    commited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,24 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch("http://localhost:4000/users")
-      .then(r => r.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .then((r) => r.json())
+      .then((data) => setUsers(data));
   }, []);
-  
-  const userList = users.map(user =>{
-    return <UserCard key={user.id} user={user}/>
-  });
+
+  const userList = users.map((user) => (
+    <UserCard key={user.id} user={user} />
+  ));
 
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
@@ -26,6 +26,6 @@ function Home() {
       </main>
     </>
   );
-};
+}
 
 export default Home;
